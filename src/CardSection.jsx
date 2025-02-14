1dd7885dfce58d98efbcea248c6c74772b8b0e2d
Abdul Rahman Qosim <157493741+rcplaneboss@users.noreply.github.com>
2025-02-14 08:11:24 +0100

    Update CardSection.jsx

--- src/CardSection.jsx
+++ src/CardSection.jsx
@@ -5,32 +5,32 @@ const CardSection = () => {
         {
             heading: 'Flexible Learning Schedule',
             para: 'Students can learn at their own pace and choose times that fit their schedules, making it easier for working professionals, students, or those with family commitments.',
-            icon: 'fa fa-phone'
+            icon: 'fa-solid fa-book'
         },
         {
             heading: 'Personalized Attention',
             para: 'With one-on-one or small group sessions, students receive more personalized guidance compared to traditional classroom settings.',
-            icon: 'fa fa-phone'
+            icon: 'fa-solid fa-bell'
         },
         {
             heading: 'Qualified Instructors',
             para: 'Your program likely provides access to experienced teachers who specialize in Quranic memorization (Hifz), Arabic language, and Islamic Studies, ensuring high-quality education.',
-            icon: 'fa fa-phone'
+            icon: ' fa-solid fa-thumbs-up '
         },
         {
             heading: 'Comprehensive Curriculum',
             para: 'Your school covers multiple aspects of Islamic education, from mastering Quranic recitation and memorization to understanding Arabic and foundational Islamic knowledge.',
-            icon: 'fa fa-phone'
+            icon: 'fa-solid fa-arrows'
         },
         {
             heading: 'Accessible from Anywhere',
             para: 'With an online platform, students can join from anywhere in the world, removing geographical barriers to gaining Islamic knowledge.',
-            icon: 'fa fa-phone'
+            icon: 'fa-solid fa-user'
         },
         {
             heading: 'Islamic Environment',
             para: 'The focus on Islamic values, morals, and ethics in your teaching helps students to not only learn the Quran and Arabic but also to grow spiritually and build character in accordance with Islamic principles.',
-            icon: 'fa fa-phone'
+            icon: 'fa-solid fa-leaf'
         },
     ]
     return (
