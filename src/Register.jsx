955d7de4644827f5cfe92c7102a2ef79a67158fe
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-02-14 17:23:53 +0100

    corrected endpoint

--- src/Register.jsx
+++ src/Register.jsx
@@ -96,7 +96,7 @@ const Register = () => {
           console.log(response.data);
         })
         .catch((error) => {
-          console.log(error);
+          console.log(response.data);
         });
     }
   }, [dataObject]);
