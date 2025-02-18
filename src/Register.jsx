43b22a4ba1b124c94d50e2bc35d21c8f1b7cc3ab
Abdul Rahman Qosim <157493741+rcplaneboss@users.noreply.github.com>
2025-02-18 11:43:20 +0100

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -79,7 +79,7 @@ const Register = () => {
 
     if (dataObject !== null) {
       axios
-        .post("https://alitqanonlineschool.rf.gd/proxy.php", qs.stringify(dataObject))
+        .post("https://alitqanonlineschool.atwebpages.com", qs.stringify(dataObject))
         .then((response) => {
           if (
             !Object.values(response?.data?.errors).some((value) =>
