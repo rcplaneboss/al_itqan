1b7d33538e52bd1a0df688dadc64d567a3967054
Abdul Rahman Qosim <157493741+rcplaneboss@users.noreply.github.com>
2025-02-24 13:43:32 +0100

    Update LogIn.jsx

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -41,7 +41,7 @@ const LogIn = () => {
 
    useEffect(() => {
       if (data != null) {
-         axios.post("https://alitqanonlineschool.rf.gd/views/login_admin.php", qs.stringify(data)).then((response) => {
+         axios.post("https://al-itqan-backend.onrender.com/views/login_admin.php", qs.stringify(data)).then((response) => {
             if (!response.data.check_status) {
                setLogIn(true);
                console.log(response.data);
